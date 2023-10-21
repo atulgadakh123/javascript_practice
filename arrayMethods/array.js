cdd2c75c646c0c968dce1a2bb1ceccef276a13c2
Atul Gadakh <atulgadakh1998@gmail.com>
2023-10-21 11:04:21 +0530

    connection between html and js

--- arrayMethods/array.js
+++ arrayMethods/array.js
@@ -1,7 +1,7 @@
 const roomsNo = [1,2,3,4,5];
 
-const ele = roomsNo.map((data) => {
-    return data;
+const ele = roomsNo.map((No) => {
+    return No;
 })
 
-console.log("array element",ele);
+console.log("array element ====>",ele);
